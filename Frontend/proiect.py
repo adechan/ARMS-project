2a8ebb2d584c6d81f6969778eb77144450a7f172
Ana Tiganescu <48323519+anatiganescu14@users.noreply.github.com>
2020-03-25 13:00:54 +0200

    Update proiect.py

--- Frontend/proiect.py
+++ Frontend/proiect.py
@@ -6,4 +6,4 @@ def main():
     return render_template("interfata.html")
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=10)
+    app.run(debug=True, host="localhost", port=10)
